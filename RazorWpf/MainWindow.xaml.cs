6187bbafa2e15c1669de14fa168a2d9d280317d3
rustamse <rustamse@gmail.com>
2017-01-15 20:10:52 +0500

    Добавил простейшую Wpf программу для проверки работоспособности (реализовал в ней пока не все функции).

--- RazorWpf/MainWindow.xaml.cs
+++ RazorWpf/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RazorCore.Cash;
+using RazorCore.Subscription;
 
 namespace RazorWpf
 {
@@ -20,9 +11,59 @@ namespace RazorWpf
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private readonly CashController _cashController;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			for (int i = 1; i <= 28; i++)
+			{
+				RazorComboBox.Items.Add(new ComboBoxItem { Content = i.ToString() });
+				RazorAndGelComboBox.Items.Add(new ComboBoxItem { Content = i.ToString() });
+				RazorAndGelAndFoam1ComboBox.Items.Add(new ComboBoxItem { Content = i.ToString() });
+				RazorAndGelAndFoam2ComboBox.Items.Add(new ComboBoxItem { Content = i.ToString() });
+			}
+
+			RazorComboBox.SelectedIndex = 0;
+			RazorAndGelComboBox.SelectedIndex = 0;
+			RazorAndGelAndFoam1ComboBox.SelectedIndex = 0;
+			RazorAndGelAndFoam2ComboBox.SelectedIndex = 14;
+
+			var priceList = new PriceList(1, 9, 19);
+			_cashController = new CashController(DateTime.Now.Date, priceList);
+
+			DatePicker.SelectedDateChanged += DatePickerOnSelectedDateChanged;
+		}
+
+		private void DatePickerOnSelectedDateChanged(object sender, SelectionChangedEventArgs selectionChangedEventArgs)
+		{
+			_cashController.CurrentDate = DatePicker.DisplayDate;
+			CashTxt.Text = ((int)_cashController.CalculateTotalCash()).ToString();
+		}
+
+		private void OnMakeSubscriptionClick(object sender, RoutedEventArgs e)
+		{
+			SubscriptionPlan subscriptionPlan = null;
+			if (razorCheckBox.IsChecked == true)
+			{
+				subscriptionPlan = new SubscriptionPlan(SubscriptionTypes.Razor, DeliveryRegularity.OncePerMonth,
+					new DeliveryTime(RazorComboBox.SelectedIndex + 1));
+			}
+			if (razorAndGelCheckBox.IsChecked == true)
+			{
+				subscriptionPlan = new SubscriptionPlan(SubscriptionTypes.RazorAndGel, DeliveryRegularity.OncePerMonth,
+					new DeliveryTime(RazorAndGelComboBox.SelectedIndex + 1));
+			}
+			else if (razorAndGelAndFoamCheckBox.IsChecked == true)
+			{
+				subscriptionPlan = new SubscriptionPlan(SubscriptionTypes.RazorAndGelAndFoam, DeliveryRegularity.OncePerMonth,
+					new DeliveryTime(RazorAndGelAndFoam1ComboBox.SelectedIndex + 1, RazorAndGelAndFoam2ComboBox.SelectedIndex + 1));
+			}
+
+			_cashController.SetSubscriptionPlan(subscriptionPlan);
+
+			CashTxt.Text = ((int)_cashController.CalculateTotalCash()).ToString();
 		}
 	}
 }
---
+++ RazorWpf/PriceList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RazorCore.Cash;
+using RazorCore.Subscription;
+
+namespace RazorWpf
+{
+	public class PriceList : IPriceList
+	{
+		private readonly double _razorPrice;
+		private readonly double _razorAndGelPrice;
+		private readonly double _razorAndGelAndFoamPrice;
+
+		public PriceList(double razorPrice, double razorAndGelPrice, double razorAndGelAndFoamPrice)
+		{
+			_razorPrice = razorPrice;
+			_razorAndGelPrice = razorAndGelPrice;
+			_razorAndGelAndFoamPrice = razorAndGelAndFoamPrice;
+		}
+
+		public double GetPrice(SubscriptionTypes subscriptionType)
+		{
+			if (subscriptionType == SubscriptionTypes.Razor)
+				return _razorPrice;
+			if (subscriptionType == SubscriptionTypes.RazorAndGel)
+				return _razorAndGelPrice;
+			if (subscriptionType == SubscriptionTypes.RazorAndGelAndFoam)
+				return _razorAndGelAndFoamPrice;
+
+			throw new ArgumentOutOfRangeException(nameof(subscriptionType));
+		}
+	}
+}
